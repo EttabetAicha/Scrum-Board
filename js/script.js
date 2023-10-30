28c0d841135f95f4deba8c87f0791691096d6cc9
aicha <aicha70tabite@gmail.com>
2023-10-30 10:02:35 +0100

    add const parts

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const body = document.querySelector('body'),
+sidebar = body.querySelector('.sidebar'),
+toggle = body.querySelector(".toggle")
+toggle.addEventListener("click" , () =>{
+sidebar.classList.toggle("close");
+})
