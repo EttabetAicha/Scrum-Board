d650bae8073b98ba256e81c97af3d698066c1683
HMZElidrissi <hamza.ezzharelidrissi1@gmail.com>
2023-10-31 15:16:20 +0100

    implementing adding and deleting users

--- js/script.js
+++ js/script.js
@@ -1,6 +1,42 @@
-const body = document.querySelector('body'),
-sidebar = body.querySelector('.sidebar'),
-toggle = body.querySelector(".toggle")
-toggle.addEventListener("click" , () =>{
-sidebar.classList.toggle("close");
-})
+const body = document.querySelector("body"),
+  sidebar = body.querySelector(".sidebar"),
+  toggle = body.querySelector(".toggle");
+toggle.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
+
+function addUsers() {
+  let name = $("#addUsers #name").val();
+  let email = $("#addUsers #email").val();
+  let role = $("#addUsers select").val();
+
+  let newRow =
+    "<tr>" +
+    '<th scope="row"><img src="/images/avatar2.png"></th>' +
+    "<td>" +
+    name +
+    "</td>" +
+    "<td>" +
+    email +
+    "</td>" +
+    "<td>" +
+    role +
+    "</td>" +
+    "<td>" +
+    '<svg class="feather feather-edit" data-toggle="modal" data-target="#editUser" fill="#85B6FF" height="20" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>' +
+    '<svg data-toggle="modal" data-target="#deleteUser" fill="#F24E1E" height="24" viewBox="0 0 48 48" width="48" xmlns="http://www.w3.org/2000/svg"><path d="M12 38c0 2.21 1.79 4 4 4h16c2.21 0 4-1.79 4-4V14H12v24zM38 8h-7l-2-2H19l-2 2h-7v4h28V8z"></path><path d="M0 0h48v48H0z" fill="none"></path></svg>' +
+    "</td>" +
+    "</tr>";
+
+  $("table tbody").append(newRow);
+
+  $("#addUsers").modal("hide");
+  return false;
+}
+
+function deleteUser() {
+  if (confirm("Are you sure you want to delete this user?")) {
+    const user = event.target.parentElement.parentElement.parentElement;
+    user.remove();
+  }
+}
