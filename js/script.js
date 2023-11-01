cf6cd22cec593a9e4d14f93885ef428acc38f0b1
youness <younesserbai@gmail.com>
2023-11-01 17:57:25 +0100

    add the dara&drop to page

--- js/script.js
+++ js/script.js
@@ -5,15 +5,20 @@ toggle.addEventListener("click", () => {
   sidebar.classList.toggle("close");
 });
 // -----------------------------------------------------------------
-// let para = document.getElementsByClassName("para");
+let para = document.getElementsByClassName("para");
 let plus = document.getElementById("plus");
 let inp = document.getElementById("inputValu");
 let bx_test = document.getElementById("bx_test");
+
+let boxs = document.querySelectorAll(".your_sprint");
+
+let drag = null;
 plus.onclick = function () {
   if (inp.value != "") {
-    bx_test.innerHTML += `<p class="item" draggable="true">${inp.value}</p>`;
+    bx_test.innerHTML += `<p class="item"  draggable="true">${inp.value}</p>`;
     inp.value = "";
   }
+  dragItem();
 };
 // input
 function toggleElement() {
@@ -47,4 +52,36 @@ function shin() {
     // If the element is currently visible, hide it
     sin.style.display = "none";
   }
+}
+
+//_______________________________drag and drop______________
+function dragItem() {
+  let items = document.querySelectorAll(".item");
+  items.forEach((item) => {
+    item.addEventListener("dragstart", function () {
+      drag = item;
+      console.log("drag start");
+    });
+
+    item.addEventListener("dragend", function () {
+      drag = null;
+      console.log("drag end");
+    });
+    boxs.forEach((even) => {
+      even.addEventListener("dragover", function (event) {
+        event.preventDefault();
+        this.style.background = "rgba(0, 0, 0, 0.04)";
+        this.style.color = "rgba(0, 0, 0, 0.7)";
+      });
+      even.addEventListener("dragleave", function () {
+        this.style.background = "white ";
+        this.style.color = "rgba(0, 0, 0, 0.721)";
+      });
+      even.addEventListener("drop", function () {
+        this.append(drag);
+        this.style.background = "white ";
+        this.style.color = "rgba(0, 0, 0, 0.721)";
+      });
+    });
+  });
 }
