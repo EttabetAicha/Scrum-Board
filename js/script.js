c41e22e964db0ae9ae784bab52827474e7f67160
HMZElidrissi <hamza.ezzharelidrissi1@gmail.com>
2023-11-02 09:33:08 +0100

    delete functionality needs to be fixed

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ function addUsers() {
     role +
     "</td>" +
     "<td>" +
-    '<svg class="feather feather-edit" data-toggle="modal" data-target="#editUser" onclick="prepareEditUser(this)" fill="#85B6FF" height="20" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"> <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" /> <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" /> </svg>' +
+    '<svg class="feather feather-edit" data-toggle="modal" data-target="#editUser" onclick="prepareToEdit(this)" fill="#85B6FF" height="20" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"> <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" /> <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" /> </svg>' +
     '<svg onclick="deleteUser()" fill="#F24E1E" height="24" viewBox="0 0 48 48" width="48" xmlns="http://www.w3.org/2000/svg"> <path d="M12 38c0 2.21 1.79 4 4 4h16c2.21 0 4-1.79 4-4V14H12v24zM38 8h-7l-2-2H19l-2 2h-7v4h28V8z" /> <path d="M0 0h48v48H0z" fill="none" /> </svg>' +
     "</td>" +
     "</tr>";
@@ -34,14 +34,19 @@ function addUsers() {
   return false;
 }
 
-function deleteUser() {
-  if (confirm("Are you sure you want to delete this user?")) {
-    const user = event.target.parentElement.parentElement.parentElement;
-    user.remove();
-  }
+function prepareToDelete(el) {
+  let index = $(el).data("index");
+  $("#deleteUser #rowIndex").val(index);
 }
 
-function prepareEditUser(el) {
+function deleteUser() {
+  let index = $("#deleteUser").data("index");
+  $("#userTableBody tr:eq(" + index + ")").remove();
+
+  $("#deleteUser").modal("hide");
+}
+
+function prepareToEdit(el) {
   let index = $(el).data("index");
   let row = $("#userTableBody tr:eq(" + index + ")");
   let name = row.find("td:eq(0)").text();
