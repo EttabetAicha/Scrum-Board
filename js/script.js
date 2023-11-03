9ae0995eb22f06fc3fb108763e8b05f22d76d62a
VoidGravity <abdellahbardichwork@gmal.com>
2023-11-03 10:11:46 +0100

    adding the responsive

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ toggle.addEventListener("click" , () =>{
 sidebar.classList.toggle("close");
 })
 document.body.style.overflow = "hidden";
+//making the toggle popup
+var popup = document.getElementsByClassName("board-BgLayer")[0];
+var closePopup= document.getElementsByClassName("close-board-popup")[0];
+var card = document.getElementsByClassName("card-body");
+
+closePopup.addEventListener('click', closeP);
+for (var i=0; i<card.length;i++) {
+    card[i].addEventListener('click', openP);
+}
+function closeP () {
+    popup.style.display = "none";
+}
+function openP () {
+    popup.style.display = "flex";
+}
+//making the drag and drop function
